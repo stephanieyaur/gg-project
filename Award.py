6147be3a4b191d7c6607c05298dacd81fce0f344
Stephanie <stephanieyaur2022@u.northwestern.edu>
2023-01-22 02:19:10 -0600

    Initial setup: create requirement.txt, GoldenGlobes() and Award() classes, write pre_ceremony

---
+++ Award.py
@@ -0,0 +1,6 @@
+class Award():
+    def __init__(self):
+        self.award_name = None #string
+        self.presenters = [] #list of strings
+        self.nominees = [] #list of strings
+        self.winners = [] #list of strings
---
+++ GoldenGlobe.py
@@ -0,0 +1,4 @@
+class GoldenGlobe():
+    def __init__(self):
+        self.hosts = [] #list of strings
+        self.awards = [] #list of Award classes
